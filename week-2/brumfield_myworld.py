7e84c8804203da5b315582e8e9e437e0cd88949b
jb830 <jbrumfield623@gmail.com>
2024-01-20 11:32:19 -0800

    added week-2 assignments

---
+++ week-2/brumfield_myworld.py
@@ -0,0 +1,16 @@
+"""""
+============================================
+; Title:  brumfield_myworld.py 
+; Author: Professor Krasso
+; Date: 20, January, 2024
+; Modified by: Joanna Brumfield
+; Description: Pthon Variables
+;===========================================
+"""""
+
+welcome_msg = 'You are now in '
+last_name = "Brumfield's"
+welcome_msg_end = " World!" 
+message = print(welcome_msg + last_name + welcome_msg_end)
+
+message
