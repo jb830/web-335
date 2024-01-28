a3b657d721613a44d6c635b2b428bbe4f74355fd
jb830 <jbrumfield623@gmail.com>
2024-01-28 08:13:21 -0800

    completed 3.3

--- week-3/brumfield_calculator.py
+++ week-3/brumfield_calculator.py
@@ -8,13 +8,6 @@
 ;===========================================
 """""
 
-# Create a function named add with two parameters and in the body of the method return the total.
-# Create a function named subtract with two parameters and in the body of the method return the subtracted total. 
-# Create a function named divide with two parameters and in the body of the method return the divided total.
-# Create a function named multiply with two parameters and in the body of the method return the multiplied total.
-# Create variables to test each function.  Use a variable to build a string for the results.  Use the format <num1 [arithmetic operation] num2 is num3.  For example, 
-
-# Call each function passing in the variables created in step 6 and print the results to the console window using an output variable and string concatenation.  
 #addition function
 def add(num1, num2):
     total = num1 + num2
@@ -35,8 +28,16 @@ def multiply(num1, num2):
     total = num1 * num2
     return total
     
-#test functions
-print(add(10,12))
-print(subtract(24,10))
-print(divide(24,3))
-print(multiply(2,8))
+#create variables to test and print functions
+num1 = 30
+num2= 10
+
+addition = add(num1, num2)
+subtraction = subtract(num1,num2)
+division = divide(num1,num2)
+multiplication = multiply(num1,num2)
+
+print(f"{num1} plus {num2} is {addition}")
+print(f"{num1} minus {num2} is {subtraction}")
+print(f"{num1} divived by {num2} is {division}")
+print(f"{num1} multiplied by {num2} is {multiplication}")
