11b152c163e37d672240359320ff2560a9154674
jb830 <jbrumfield623@gmail.com>
2024-02-09 16:15:55 -0800

    added comments and code attribution to assignment 5.2

--- week-5/brumfield-5.2.js
+++ week-5/brumfield-5.2.js
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title:  brumfield_myworld.py 
+; Author: Professor Krasso
+; Date: 28, January, 2024
+; Modified by: Joanna Brumfield
+; Description: Pthon Math Functions
+;===========================================
+*/
+
+//insert a new user to users collection 
 db.users.insertOne({
     "firstName": "Antonio",
     "lastName": "Vivaldi",
@@ -6,7 +17,7 @@ db.users.insertOne({
     "dateCreated": new Date()
 });
 
- 
+//update email based on user _id 
 db.users.updateOne(
     { "_id" : ObjectId("65bd2944f2e20dd3a02383e8")},
     {   
@@ -15,6 +26,7 @@ db.users.updateOne(
     }
 );
 
+//show only firstName, lastName, and email fields for all documents in users collection
 db.users.aggregate (
     { $match: {} },
     { $project: { _id: 0, firstName: 1, lastName: 1, email: 1 } }
