98aabc7a11a42059ddd4525a136c5cba3fbe2b9f
jb830 <jbrumfield623@gmail.com>
2024-02-18 15:48:35 -0800

    Added 6.2 and 6.3

---
+++ week-6/brumfield-6.2.js
@@ -0,0 +1,66 @@
+/*
+============================================
+; Title:  brumfield_6.2
+; Author: Professor Krasso
+; Date: 18, February, 2024
+; Modified by: Joanna Brumfield
+; Description: MongoDB Aggregate Queries
+;===========================================
+*/
+// Write a query to show a list of documents in the houses collection.  Take a screenshot of the output and save it as an image.
+db.houses.find()
+// Write a query to show a list of documents in the student’s collection.  Take a screenshot of the output and save it as an image.
+db.students.find()
+// Write a query to add a document to the student’s collection.  Take a screenshot of the output and save it as an image.  Next, write a query to prove the document was added to the user’s collection.  Take a screenshot of the output and save it as an image. 
+db.students.insertOne({
+    "firstName": "Jane",
+    "lastName": "Smith",
+    "studentId": "g7845",
+    "houseId": "h8547"
+})
+// Write a query to delete the document you created in step 4.  Take a screenshot of the output and save it as an image.  Next, write a query to prove the document was added to the user’s collection.  Take a screenshot of the output and save it as an image.
+db.students.remove({
+    _id: ObjectId('65d273d4d9b9b6335543770c')
+})
+// Write a query to show a list of students by house (hint: use the lookup operation).  Take a screenshot of the output and save it as an image. 
+db.students.aggregate({
+    $lookup: {
+        from: "houses",
+        localField: "houseId",
+        foreignField: "houseId",
+        as: "studentsByHouse"
+    }
+})
+//  Write a query to show a list of students for house Gryffindor (hint: use the lookup and match operations).  Take a screenshot of the output and save it as an image. 
+db.students.aggregate([
+    {
+      $match: {
+        "houseId": "h1007"
+      }
+    },
+    {
+      $lookup: {
+        from: "houses",
+        localField: "houseId",
+        foreignField: "houseId",
+        as: "studentsByGryffindor" 
+      }
+    }
+  ])
+  
+// Write a query to show a list of students for the Eagle mascot (hint: use the lookup and match operations).  Take a screenshot of the output and save it as an image. 
+db.students.aggregate([
+    {
+      $match: {
+        "mascot": "Eagle"
+      }
+    },
+    {
+      $lookup: {
+        from: "houses",
+        localField: "houseId",
+        foreignField: "houseId",
+        as: "EagleMascot" 
+      }
+    }
+  ])
---
+++ week-6/brumfield_usersp1.py
@@ -0,0 +1,29 @@
+"""""
+============================================
+; Title:  brumfield_usersp1.py 
+; Author: Professor Krasso
+; Date: 18, February, 2024
+; Modified by: Joanna Brumfield
+; Description: Python with MongoDB, Part 1
+;===========================================
+"""""
+
+"Using the “Python Guide” document I provided as a guide, build a Python program that connects to your web335DB database."
+# Import Mongo
+from pymongo import MongoClient
+
+# Connection string
+client = MongoClient("mongodb+srv://web335_user:s3cret@web335db.xempkut.mongodb.net/web335DB?retryWrites=true&w=majority")
+
+# Variable to access web335DB
+db = client['web335DB']
+
+"Write the Python code to display all documents in the user’s collection."
+# for user in db.users.find():
+#     print(user)
+# "Write the Python code to display a document where the employeeId is 1011."
+# for user in db.users.find({"employeeId": "1011"}):
+#     print(user)
+"Write the Python code to display a document where the lastName is Mozart."
+for user in db.users.find({"lastName": "Mozart"}):
+    print(user)
